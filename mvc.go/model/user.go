2b1c18628d809c2499c8b4ae9386cca676f43fa6
aimaragomez <2114782@ucc.edu.ar>
2023-05-30 14:56:06 -0300

    .

---
+++ mvc.go/model/user.go
@@ -0,0 +1,16 @@
+package model
+
+type User struct {
+	Id       int    `gorm:"primaryKey"`
+	Name     string `gorm:"type:varchar(350);not null"`
+	LastName string `gorm:"type:varchar(250);not null"`
+	UserName string `gorm:"type:varchar(150);not null;unique"`
+	Email    string `gorm:"type:varchar(250);not null;unique"`
+	Password string `gorm:"type:varchar(150);not null"`
+	IsAdmin  bool   `gorm:"default:false"`
+
+	Address   Address `gorm:"foreignkey:AddressId"`
+	AddressId int
+
+	Telephones Telephone `gorm:"foreignKey:UserId"`
+}
