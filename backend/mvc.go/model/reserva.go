49aec117f30a811159bbdceaa46a5cf296d29115
aimaragomez <2114782@ucc.edu.ar>
2023-05-31 17:51:17 -0300

    *

--- backend/mvc.go/model/reserva.go
+++ backend/mvc.go/model/reserva.go
@@ -4,12 +4,11 @@ import (
 	"time"
 )
 
-// Reserva representa una reserva de habitación
 type Reserva struct {
-	Id       int       `gorm:"primaryKey"`            // Identificador único de la reserva (clave primaria)
+	Id       int       `gorm:"primaryKey"`
 	DateFrom time.Time `gorm:"type:date;not null"`    // Fecha de inicio de la reserva
 	DateTo   time.Time `gorm:"type:date;not null"`    // Fecha de finalización de la reserva
-	Duracion int       `gorm:"type:integer;not null"` // Duración de la reserva en días
+	Duracion int       `gorm:"type:integer;not null"` // Duración de la reserva
 	Precio   float64   `gorm:"type:double; not null"` // Precio de la reserva
 
 	UserId  int // Identificador del usuario asociado a la reserva
