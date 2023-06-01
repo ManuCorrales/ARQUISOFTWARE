be83ad29ccf1ff693ac0a0c5f333dc827a3b8840
aimaragomez <2114782@ucc.edu.ar>
2023-06-01 10:54:50 -0300

    /

--- backend/mvc-go/sevices/user_service.go
+++ backend/mvc-go/sevices/user_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	userCliente "mvc-go/clients/user"
 	"mvc-go/dto"
 	e "mvc-go/utils/errors"
 )
@@ -12,4 +13,33 @@ type userServiceInterface interface {
 	GetUsersByUsername(username string) (dto.UsersDto, e.ApiError)
 	InsertUser(userDto dto.UserDto) (dto.UserDto, e.ApiError)
 	GetUsers() (dto.UsersDto, e.ApiError)
+}
+
+var (
+	UserService userServiceInterface
+)
+
+func (s *userService) GetUserById(id int) (dto.UserDto, e.ApiError) {
+
+	var user model.User = userCliente.GetUserById(id)
+	var userDto dto.UserDto
+
+	if user.Id == 0 {
+		return userDto, e.NewBadRequestApiError("user not found")
+	}
+
+	userDto.Name = user.Name
+	userDto.LastName = user.LastName
+	userDto.Street = user.Address.Street
+	userDto.Number = user.Address.Number
+	for _, telephone := range user.Telephones {
+		var dtoTelephone dto.TelephoneDto
+
+		dtoTelephone.Code = telephone.Code
+		dtoTelephone.Number = telephone.Number
+
+		userDto.TelephonesDto = append(userDto.TelephonesDto, dtoTelephone)
+	}
+
+	return userDto, nil
 }
