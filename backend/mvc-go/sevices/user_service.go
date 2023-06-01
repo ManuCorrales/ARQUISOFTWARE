f3c09368dc2ebc7405fcab47924eb292a84490d4
aimaragomez <2114782@ucc.edu.ar>
2023-06-01 10:48:42 -0300

    /

--- backend/mvc-go/sevices/user_service.go
+++ backend/mvc-go/sevices/user_service.go
@@ -1,1 +1,15 @@
 package services
+
+import (
+	"mvc-go/dto"
+	e "mvc-go/utils/errors"
+)
+
+type userService struct{}
+
+type userServiceInterface interface {
+	GetUserById(id int) (dto.UserDto, e.ApiError)
+	GetUsersByUsername(username string) (dto.UsersDto, e.ApiError)
+	InsertUser(userDto dto.UserDto) (dto.UserDto, e.ApiError)
+	GetUsers() (dto.UsersDto, e.ApiError)
+}
