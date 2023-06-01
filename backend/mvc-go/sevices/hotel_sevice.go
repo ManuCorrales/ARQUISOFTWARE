a4192f3ecec5112c1ecf69cee6d682ef08444cd7
aimaragomez <2114782@ucc.edu.ar>
2023-06-01 14:33:50 -0300

    /

--- backend/mvc-go/sevices/hotel_sevice.go
+++ backend/mvc-go/sevices/hotel_sevice.go
@@ -1,1 +1,99 @@
-package service
+package services
+
+import (
+	hotelClient "mvc-go/Clients/hotel"
+
+	"mvc-go/dto"
+	"mvc-go/model"
+	e "mvc-go/utils/errors"
+)
+
+type hotelService struct{}
+
+type hotelServiceInterface interface {
+	GetHotelById(id int) (dto.HotelDto, e.ApiError)
+	GetHotels() (dto.HotelsDto, e.ApiError)
+	InsertHotel(hotelDto dto.HotelDto) (dto.HotelDto, e.ApiError)
+}
+
+var (
+	HotelService hotelServiceInterface
+)
+
+func init() {
+	HotelService = &hotelService{}
+}
+
+func (h *hotelService) GetHotelById(id int) (dto.HotelDto, e.ApiError) {
+
+	var hotel model.Hotel = hotelClient.GetHotelById(id)
+	var hotelDto dto.HotelDto
+
+	if hotel.ID == 0 {
+		return hotelDto, e.NewBadRequestApiError("no se encontro la reserva")
+	}
+	hotelDto.ID = hotel.ID
+	hotelDto.Name = hotel.Name
+	hotelDto.Availability = hotel.Availability
+	hotelDto.Description = hotel.Description
+	hotelDto.Email = hotel.Email
+	hotelDto.Telephone = hotel.Telephone
+	hotelDto.Rooms = hotel.Rooms
+	hotelDto.Image = hotel.Image
+
+	for _, reserva := range hotel.Reservas {
+		var dtoreserva dto.ReservaDto
+
+		dtoreserva.DateFrom = reserva.DateFrom
+		dtoreserva.DateTo = reserva.DateTo
+		dtoreserva.Duracion = reserva.Duracion
+		dtoreserva.Precio = reserva.Precio
+		dtoreserva.HotelId = reserva.HotelId
+
+		hotelDto.ReservasDto = append(hotelDto.ReservasDto, dtoreserva)
+	}
+
+	return hotelDto, nil
+}
+
+func (h *hotelService) GetHotels() (dto.HotelsDto, e.ApiError) {
+
+	var hotels model.Hotels = hotelClient.Gethotels()
+	var hotelsDto dto.HotelsDto
+
+	for _, hotel := range hotels {
+		var hotelDto dto.HotelDto
+
+		hotelDto.ID = hotel.ID
+		hotelDto.Availability = hotel.Availability
+		hotelDto.Description = hotel.Description
+		hotelDto.Email = hotel.Email
+		hotelDto.Name = hotel.Name
+		hotelDto.Telephone = hotel.Telephone
+		hotelDto.Rooms = hotel.Rooms
+		hotelDto.Image = hotel.Image
+
+		hotelsDto = append(hotelsDto, hotelDto)
+	}
+
+	return hotelsDto, nil
+}
+
+func (h *hotelService) InsertHotel(hotelDto dto.HotelDto) (dto.HotelDto, e.ApiError) {
+
+	var hotel model.Hotel
+
+	hotel.Description = hotelDto.Description
+	hotel.Email = hotelDto.Email
+	hotel.Name = hotelDto.Name
+	hotel.Availability = hotelDto.Availability
+	hotel.Telephone = hotelDto.Telephone
+	hotel.Rooms = hotelDto.Rooms
+	hotel.Image = hotelDto.Image
+
+	hotel = hotelClient.Inserthotel(hotel)
+
+	hotelDto.ID = hotel.ID
+
+	return hotelDto, nil
+}
--- backend/mvc-go/sevices/user_service.go
+++ backend/mvc-go/sevices/user_service.go
@@ -1,7 +1,8 @@
 package services
 
 import (
-	userCliente "mvc-go/clients/user"
+	userClient "mvc-go/Client/user"
+
 	"mvc-go/dto"
 	"mvc-go/model"
 	e "mvc-go/utils/errors"
@@ -9,38 +10,115 @@ import (
 
 type userService struct{}
 
+// interface del usuario
 type userServiceInterface interface {
 	GetUserById(id int) (dto.UserDto, e.ApiError)
-	GetUsersByUsername(username string) (dto.UsersDto, e.ApiError)
-	InsertUser(userDto dto.UserDto) (dto.UserDto, e.ApiError)
+	GetUserByUsername(username string) (dto.UserDto, e.ApiError)
 	GetUsers() (dto.UsersDto, e.ApiError)
+	InsertUser(userDto dto.UserDto) (dto.UserDto, e.ApiError)
 }
 
+// inicializo una variable de service para poder usar sus metodos
 var (
 	UserService userServiceInterface
 )
 
-func (s *userService) GetUserById(id int) (dto.UserDto, e.ApiError) {
+func init() {
+	UserService = &userService{}
+}
 
-	var user model.User = userCliente.GetUserById(id)
+func (u *userService) GetUserById(id int) (dto.UserDto, e.ApiError) {
+
+	var user model.User = userClient.GetUserById(id)
+	//declaro un dto de usuario
 	var userDto dto.UserDto
 
+	//si no lo trae, me da error de que no encontró el usuario
 	if user.Id == 0 {
-		return userDto, e.NewBadRequestApiError("user not found")
+		return userDto, e.NewBadRequestApiError("no se ha encontrado el usuario")
 	}
 
+	userDto.Id = user.Id
 	userDto.Name = user.Name
 	userDto.LastName = user.LastName
-	userDto.Street = user.Address.Street
-	userDto.Number = user.Address.Number
-	for _, telephone := range user.Telephones {
-		var dtoTelephone dto.TelephoneDto
+	userDto.UserName = user.UserName
+	userDto.Password = user.Password
+	userDto.Email = user.Email
+	userDto.IsAdmin = user.IsAdmin
 
-		dtoTelephone.Code = telephone.Code
-		dtoTelephone.Number = telephone.Number
+	for _, Reserva := range user.Reserva {
+		var dtoReserva dto.ReservaDto
 
-		userDto.TelephonesDto = append(userDto.TelephonesDto, dtoTelephone)
+		dtoReserva.DateFrom = Reserva.DateFrom
+		dtoReserva.DateTo = Reserva.DateTo
+		dtoReserva.Duracion = Reserva.Duration
+		dtoReserva.Precio = Reserva.Price
+		dtoReserva.HotelId = Reserva.HotelId
+
+		userDto.ReservasDto = append(userDto.ReservasDto, dtoReserva)
 	}
+
+	return userDto, nil
+}
+
+func (u *userService) GetUserByUsername(username string) (dto.UserDto, e.ApiError) {
+
+	var user model.User = userClient.GetUserByUsername(username)
+	//declaro un dto de usuario
+	var userDto dto.UserDto
+
+	//si no lo trae, me da error de que no encontró el usuario
+	if user.UserName == "" {
+		return userDto, e.NewBadRequestApiError("no se ha encontrado el usuario")
+	}
+
+	userDto.Id = user.Id
+	userDto.Name = user.Name
+	userDto.LastName = user.LastName
+	userDto.UserName = user.UserName
+	userDto.Password = user.Password
+	userDto.Email = user.Email
+	userDto.IsAdmin = user.IsAdmin
+
+	return userDto, nil
+}
+
+func (u *userService) GetUsers() (dto.UsersDto, e.ApiError) {
+
+	var users model.User = userClient.GetUsers()
+	var usersDto dto.UsersDto
+
+	for _, user := range users {
+		var userDto dto.UserDto
+
+		userDto.Id = user.Id
+		userDto.Name = user.Name
+		userDto.LastName = user.LastName
+		userDto.UserName = user.UserName
+		userDto.Password = user.Password
+		userDto.Email = user.Email
+		userDto.IsAdmin = user.Rol
+
+		usersDto = append(usersDto, userDto)
+	}
+
+	return usersDto, nil
+}
+
+func (u *userService) InsertUser(userDto dto.UserDto) (dto.UserDto, e.ApiError) {
+
+	var user model.User
+
+	user.Name = userDto.Name
+	user.LastName = userDto.LastName
+	user.UserName = userDto.UserName
+	user.Password = userDto.Password
+	user.Email = userDto.Email
+	user.IsAdmin = userDto.IsAdmin
+
+	user = userClient.InsertUser(user)
+
+	userDto.Id = user.Id
 
 	return userDto, nil
 }
