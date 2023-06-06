64efb86ddab0fe1bb3683c6acf397158e22963ad
RoyKostrun <101714856+RoyKostrun@users.noreply.github.com>
2023-06-06 11:13:59 -0300

    add login

---
+++ frontend/src/pages/login/login.jsx
@@ -0,0 +1,17 @@
+
+
+const Login = () => {
+
+    return(
+        <div style={{ width:" 100% ", height:" 100% ", zIndex:5, alignContent: "center" }}>
+           <div >
+            <label> User: </label>
+                <input/>  
+            
+            </div> 
+            {/* list/card component  (componente que hace la lista de hoteles) */}
+        </div>
+    );
+}
+
+export default Login;
