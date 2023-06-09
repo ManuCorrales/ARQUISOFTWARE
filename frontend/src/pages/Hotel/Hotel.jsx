623d9830c3d9528c3963cfa616ef5121c94fd586
Nicolas-Casto <101714856+RoyKostrun@users.noreply.github.com>
2023-06-08 23:10:03 -0300

    front de reservas

--- frontend/src/pages/Hotel/Hotel.jsx
+++ frontend/src/pages/Hotel/Hotel.jsx
@@ -1,31 +1,77 @@
 import { useEffect, useState } from 'react'; 
 import {data} from '../../data/CarouselData'
 
+import Calendar from "@demark-pro/react-booking-calendar";
+
+
+
 
 function Hotel() {
     const [lastDirectory, setLastDirectory] = useState('');
     const [hotel, setHotel] = useState({});
+
+    const [selectedDates, setSelectedDates] = useState([]);//mandar al apretar reservar
+    const handleChange = (e) => {setSelectedDates(e);}
+    const[reserved, setReserved] = useState([
+        {
+          startDate: new Date(2023, 7, 10),
+          endDate: new Date(2023, 7, 15),
+        },
+      ])
+
+// desde el back, guardar y devolver las fechas reservadas asi:
+//un objeto asi se manda para reservarlo, las reservas para bloquearlas se tienen que mandar en un array de estos. Al guardar una reserva nueva, añadirla al fin del array
+
+
+  function reserveDate(){
+    var temp=reserved
+    temp.push({startDate:selectedDates[0],endDate:selectedDates[1]}) //en vez de guardar esto localmente se manda al back y ahi se añade al array de reservas
+    console.log(temp)
+
+  }
+
+
+
+
 
     useEffect(() => {
         const pathArray = window.location.pathname.split('/');
         const lastPath = pathArray[pathArray.length - 1];
         setLastDirectory(lastPath);
         data.forEach(element => {
-
-            if(decodeURIComponent(lastDirectory.replace(/\+/g, ' '))==element.title){setHotel(element)
- 
+            if(decodeURIComponent(lastDirectory.replace(/\+/g, ' '))==element.title){
+                setHotel(element)
             }})
-      });
-      console.log(hotel)
+    });
 
 
 
     return (
         <div style={{marginTop:"30px",width:"100vw",height:"100%",display:"flex",alignContent:"center",justifyContent:"center"}}>
-            <div style={{marginTop:"30px",width:"900px",height:"1000px", padding:"40px", borderRadius:"10px", backgroundColor:"white"}}>
+            <div style={{marginTop:"30px",width:"900px",height:"1200px", padding:"40px", borderRadius:"10px", backgroundColor:"white"}}>
                 <div style={{width:"100%",fontSize:"40px"}}>{hotel.title}</div>
                 <div style={{padding:"10px"}}>{hotel.description}</div>
-                <img src={hotel.image} style={{width:"100%"}}/>
+                <img src={hotel.image} style={{width:"100%"}}/> 
+                <div style={{border:" 1px solid gray", borderRadius:"10px",paddingTop:"20px",marginTop:"20px"}}>
+                  <Calendar
+                    style={{width:"100%"}}
+                    selected={selectedDates}
+                    onChange={handleChange}
+                    onOverbook={(e, err) => alert(err)}
+                    components={{
+                        DayCellFooter: ({ innerProps }) => (
+                             <div {...innerProps}></div>
+                                 ),
+                             }}
+                             disabled={(date, state) => !state.isSameMonth}
+                             reserved={reserved}
+                             reserverFooter={reserved}
+                             dateFnsOptions={{ weekStartsOn: 1 }}
+                             range={true}
+                             isStart={true}
+                  />
+                     <button style={{marginTop:"30px",width:"10%",position:"relative",bottom:"10px",left:"45%" }} onClick={reserveDate}> Reservar </button>
+                 </div>
             </div>
         </div>
     );
