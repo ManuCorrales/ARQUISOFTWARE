91aed1b977316bd88fef44a1bc23d7f3bd22a704
aimaragomez <2114782@ucc.edu.ar>
2023-06-11 15:17:17 -0300

    varios cambios

---
+++ frontend/src/Rutas/Navigate.jsx
@@ -0,0 +1,4 @@
+import { lazy } from "react";
+import { Home } from '../paginas/Home/Home';
+import { Login } from "../Login/Login"
+import { Register } from "..paginas/Register/Register"
--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useContext } from "react";
+
 const Register = () => {
 
     return(
