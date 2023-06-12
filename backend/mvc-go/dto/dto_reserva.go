f2a3a4c94152b01c2ca428c884214aa6826e007f
aimaragomez <2114782@ucc.edu.ar>
2023-06-12 20:04:15 -0300

    cambios

--- backend/mvc-go/dto/dto_reserva.go
+++ backend/mvc-go/dto/dto_reserva.go
@@ -8,7 +8,6 @@ type ReservaDto struct {
 	Id       int       `json:"id"`
 	DateFrom time.Time `json:"date_from"` // Fecha de inicio de la reserva
 	DateTo   time.Time `json:"date_to"`   // Fecha de finalización de la reserva
-	Duracion int       `json:"duracion"`  // Duración de la reserva
 	Precio   float64   `json:"precio"`    // Precio de la reserva
 
 	UserId  int `json:"user_id"`  // Identificador del usuario asociado a la reserva
